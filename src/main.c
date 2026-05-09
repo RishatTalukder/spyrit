d6a9a2a3a4d8b94a7627232ac5a4166b50d8d151
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2026-05-09 18:49:11 +0600

    .

--- src/main.c
+++ src/main.c
@@ -16,21 +16,21 @@
 int main() {
     char *source_code = "n: int = input\noutput n";
 
-    // Lexer my_lexer;
-    // init_lexer(&my_lexer, source_code);
-
-    // printf("Lexing code:\n%s\n\n", source_code);
-    // printf("%-15s | %s\n", "TOKEN KIND", "LITERAL");
-    // printf("----------------|---------\n");
-
-    // Token token;
-    // do {
-    //     token = next_token(&my_lexer);
-    //     printf("%-15s | %s\n", show_token(token.kind), token.literal);
-    // } while (token.kind != EOF_TOKEN);
-
-    // Lexer
     Lexer my_lexer;
+    init_lexer(&my_lexer, source_code);
+
+    printf("Lexing code:\n%s\n\n", source_code);
+    printf("%-15s | %s\n", "TOKEN KIND", "LITERAL");
+    printf("----------------|---------\n");
+
+    Token token;
+    do {
+        token = next_token(&my_lexer);
+        printf("%-15s | %s\n", show_token(token.kind), token.literal);
+    } while (token.kind != EOF_TOKEN);
+
+    // // Lexer
+    // Lexer my_lexer;
     init_lexer(&my_lexer, source_code);
 
     // Parser
